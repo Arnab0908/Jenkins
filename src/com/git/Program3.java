0178a39d82c6912d095a5b71df40b54b43401f0e
Arnab0908 <arnab02082002@gmail.com>
2026-03-09 12:19:57 +0530

    file updated

--- src/com/git/Program3.java
+++ src/com/git/Program3.java
@@ -2,8 +2,8 @@ package com.git;
 
 public class Program3 {
 			public static void main(String[] args) {
-				int a=5;
-				int b=5;
+				int a=100;
+				int b=50;
 				int c=a+b;
 				System.out.println(c);
 			}
